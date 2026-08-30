25e701e7aed91a3b6d2e34d0ae4a69a52a0f8405
zeuz1337 <zeuzbusiness07@gmail.com>
2026-08-30 23:52:53 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,9 +23,9 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
-        state: "you",     // this is what people actually see
-        type: 3,               // Custom
+        name: "Watching you", // required by Discord API, not shown in the client
+        state: "",     // this is what people actually see
+        type: 4,               // Custom
       },
     ],
   },
