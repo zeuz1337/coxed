34f9d60a5c182dab8721f8226a739fb1433a2874
zeuz1337 <zeuzbusiness07@gmail.com>
2026-08-30 23:51:24 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -93,7 +93,7 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#E03FD8",
+      primary: "#98578a",
       secondary: "#2F3136",
 
       // Standard status colors for success/error/warning/info messages.
